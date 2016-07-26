0cc0c7e0e819d4d67542223343921ab85db44c18
Michael Petretti <michael.petretti@skyscanner.net>
2016-07-26 20:37:26 +0200

    some examples on how to change the pictures of the video before computing similarity

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
+import copy
 import pprint
 import random
 import imageio
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-import pylab
-from skimage import data, img_as_float
 from skimage.measure import compare_ssim as ssim
+from skimage.color import rgb2grey
+from skimage.filters import gaussian
 
 
 def getImageListFromVideo():
@@ -40,11 +39,29 @@ def get_list_of_mse(main_image, all_images):
 def get_list_of_ssim(main_image, all_images):
     return [ssim(main_image, all_images[i], multichannel=True) for i in range(len(all_images))]
 
+def get_grey_scale_copies(all_images):
+    images_to_transform = copy.deepcopy(all_images)
+    return [rgb2grey(images_to_transform[i]) for i in range(len(images_to_transform))]
+
+def get_gaussian_detection_copies(all_images):
+    images_to_transform = copy.deepcopy(all_images)
+    return [gaussian(images_to_transform[i], sigma=1, multichannel=True) for i in range(len(images_to_transform))]
+
 
 if __name__ == '__main__' :
     all_images = getImageListFromVideo()
     main_image = pick_random_image(all_images)
     list_of_mse = get_list_of_mse(main_image, all_images)
     list_of_ssim = get_list_of_ssim(main_image, all_images)
+    grey_images = get_grey_scale_copies(all_images)
+    grey_list_of_ssim = get_list_of_ssim(rgb2grey(main_image), grey_images)
+    grey_list_of_mse = get_list_of_mse(rgb2grey(main_image), grey_images)
+    gaussian_images = get_gaussian_detection_copies(all_images)
+    gaussian_list_of_ssim = get_list_of_ssim(gaussian(main_image, sigma=1, multichannel=True), gaussian_images)
+    gaussian_list_of_mse = get_list_of_mse(gaussian(main_image, sigma=1, multichannel=True), gaussian_images)
     pprint.pprint(list_of_mse)
     pprint.pprint(list_of_ssim)
+    pprint.pprint(grey_list_of_ssim)
+    pprint.pprint(grey_list_of_mse)
+    pprint.pprint(gaussian_list_of_ssim)
+    pprint.pprint(gaussian_list_of_mse)
